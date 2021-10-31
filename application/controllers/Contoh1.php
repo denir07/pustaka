48fd48a9a801e9a8e63aa7874c2c162970ab60fc
denir07 <denirhmn15@gmail.com>
2021-10-31 20:11:20 +0700

    Mengerjakan latihan 1 sampai 4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Perkenalkan <br>";
+        echo "Nama Saya Deni Rahman Mahasiswa Universita Bina Sarana Informatika <br>";
+        echo "Saya memiliki hobi berolahraga";
+    }   
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Deni</li>
+        <li>Nama Belakang : Rahman</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Sumelap</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tasikmalaya</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Lari</li>
+    </ul>
